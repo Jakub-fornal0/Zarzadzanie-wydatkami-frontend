6b400b6dd03f5b2f58bcb090ce57433198f254df
Jakub-fornal0 <jakub.fornal0@onet.pl>
2022-05-26 19:11:41 +0200

    display user fist name and last name when website is load

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { FaUser } from "react-icons/fa";
 import styles from "./styles.module.css";
 
 const Login = () => {
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,23 +1,59 @@
 import styles from "./styles.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import Users from "./Users";
-import Info from "./Info";
+import { FaUser } from "react-icons/fa";
 
 const Main = () => {
   const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [userName, setUserName] = useState("");
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.reload();
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const config = {
+            method: "get",
+            url: "http://localhost:8080/api/info/name",
+            headers: {
+              "Content-Type": "application/json",
+              "x-access-token": token,
+            },
+          };
+          //wysłanie żądania o dane:
+          const { data: res } = await axios(config);
+          setUserName(res.data);
+        } catch (error) {
+          if (
+            error.response &&
+            error.response.status >= 400 &&
+            error.response.status <= 500
+          ) {
+            setError(error.response.data.message);
+            localStorage.removeItem("token");
+            window.location.reload();
+          }
+        }
+      }
+    };
+    fetchData();
+  });
+
   return (
     <div className={styles.main_container}>
       <nav className={styles.navbar}>
-        <h1>MySite</h1>
-        <button className={styles.white_btn} onClick={handleLogout}>
+        <h1>Zarządzanie wydatkami</h1>
+        <p>
+          Jesteś zalogowany jako: {userName.firstName} {userName.lastName}
+        </p>
+        <button className={styles.logout_btn} onClick={handleLogout}>
           Wyloguj się
         </button>
       </nav>
