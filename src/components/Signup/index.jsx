cafba79fcad63ad4ee4b90c6e31b297a73d49f0b
Jakub-fornal0 <jakub.fornal0@onet.pl>
2022-06-06 16:01:53 +0200

    Signup component grid bootstrap and media queries

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./styles.module.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Signup = () => {
   const [data, setData] = useState({
@@ -37,61 +39,67 @@ const Signup = () => {
 
   return (
     <div className={styles.signup_container}>
-      <div className={styles.signup_form_container}>
-        <div className={styles.left}>
-          <h1>Zaloguj się</h1>
-          <Link to="/login">
-            <button type="button" className={styles.login_btn}>
-              Logowanie
-            </button>
-          </Link>
-        </div>
-        <div className={styles.right}>
-          <form className={styles.form_container} onSubmit={handleSubmit}>
-            <h1>Rejestracja</h1>
-            <input
-              type="text"
-              placeholder="Imię"
-              name="firstName"
-              onChange={handleChange}
-              value={data.firstName}
-              required
-              className={styles.input}
-            />
-            <input
-              type="text"
-              placeholder="Nazwisko"
-              name="lastName"
-              onChange={handleChange}
-              value={data.lastName}
-              required
-              className={styles.input}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-              className={styles.input}
-            />
-            <input
-              type="password"
-              placeholder="Hasło"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
-              required
-              className={styles.input}
-            />
-            {error && <div className={styles.error_msg}>{error}</div>}
-            <button type="submit" className={styles.register_btn}>
-              Zarejestruj się
-            </button>
-          </form>
-        </div>
-      </div>
+      <Container className={styles.signup_form_container}>
+        <Row xl={1} lg={1} md={2} sm={2} xs={2}>
+          <Col className={styles.left} xl={4} lg={4} md={12} sm={12} xs={12}>
+            <h1>Masz już konto?</h1>
+            <p>
+              Teraz możesz kontrolować swoje wydatki! Zaloguj się używając
+              adresu email i hasła podanego przy rejestracji.
+            </p>
+            <Link to="/login">
+              <button type="button" className={styles.login_btn}>
+                Logowanie
+              </button>
+            </Link>
+          </Col>
+          <Col className={styles.right} xl={8} lg={8} md={12} sm={12} xs={12}>
+            <form className={styles.form_container} onSubmit={handleSubmit}>
+              <h1>Rejestracja</h1>
+              <input
+                type="text"
+                placeholder="Imię"
+                name="firstName"
+                onChange={handleChange}
+                value={data.firstName}
+                required
+                className={styles.input}
+              />
+              <input
+                type="text"
+                placeholder="Nazwisko"
+                name="lastName"
+                onChange={handleChange}
+                value={data.lastName}
+                required
+                className={styles.input}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                onChange={handleChange}
+                value={data.email}
+                required
+                className={styles.input}
+              />
+              <input
+                type="password"
+                placeholder="Hasło"
+                name="password"
+                onChange={handleChange}
+                value={data.password}
+                required
+                className={styles.input}
+              />
+              {error && <div className={styles.error_msg}>{error}</div>}
+              <button type="submit" className={styles.register_btn}>
+                Zarejestruj się
+              </button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
